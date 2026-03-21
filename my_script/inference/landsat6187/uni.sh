9c16979717c82a796f36c8f2531477bf1fdd9037
QQQ <138736626+xuanxh1@users.noreply.github.com>
2026-03-21 13:00:41 +0800

    Update uni.sh

--- my_script/inference/landsat6187/uni.sh
+++ my_script/inference/landsat6187/uni.sh
@@ -23,7 +23,7 @@ python test_folder/inference.py \
   --att_norm "instancenorm" \
   --imgs_color "gray" \
   --att_scales 3 5 \
-  --attention_layers_num 2 \
+  --attention_layers_num 4 \
   --dataset_list [your dataset path , which contain train and val subfolders] \
   --attn_type "MLA" \
   --scale_factor 1 \
